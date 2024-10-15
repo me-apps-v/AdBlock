da0627ad2973177d76aebdb7c0b694e6c945bcc3
me-apps-v <viomletka@gmail.com>
2024-10-15 17:32:45 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const slider = document.getElementById("adblock-slider");
+const thumbLabel = document.querySelector(".slider-thumb-label");
 
 slider.addEventListener("input", function() {
+    thumbLabel.style.left = `${slider.value * 2.5}px`; // Модифицируем положение
     if (slider.value == 100) {
         window.location.href = "https://apps.apple.com/us/app/ad-blocker-browser-protect/id1585053145";
     }
+});
+
+document.querySelector(".cancel-button").addEventListener("click", function() {
+    window.location.href = "https://apps.apple.com/us/app/ad-blocker-browser-protect/id1585053145";
 });
